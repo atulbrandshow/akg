92a7d49f403cbb15c8dbaa1a68580449c9bd8241
yatishprajapatbrandshow <yatish.prajapat@brandshow.in>
2025-07-19 09:24:18 +0530

    Protect Admin From Nabar and Footer

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,20 +4,31 @@ import '../styles/globals.css';
 import NavBar from '@/Components/NavBar';
 import { StickyFooter } from '@/Components';
 import Footer from '@/Components/Footer';
-import useStickyBar from '@/hooks/useStickyBar';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
-    // const showStickyBar = useStickyBar();
-    
+    const [isShowNavAndFooter, setIsShowNavAndFooter] = useState(false);
+    const router = useRouter();
+
+    useEffect(() => {
+        const hiddenPrefixes = ['/admin']; // only prefix, no *
+
+        const shouldHide = hiddenPrefixes.some(prefix => 
+            router.pathname === prefix || router.pathname.startsWith(prefix + '/')
+        );
+
+        setIsShowNavAndFooter(!shouldHide);
+    }, [router.pathname]);
+
     return (
         <>
-            <NavBar />
+            {isShowNavAndFooter && <NavBar />}
             <Component {...pageProps} />
-            {/* <StickyFooter ShowState={showStickyBar} /> */}
-            <StickyFooter  />
-            <Footer />
+            {isShowNavAndFooter && <StickyFooter />}
+            {isShowNavAndFooter && <Footer />}
         </>
-    )
+    );
 }
 
 export default MyApp;

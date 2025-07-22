f29767ef001c28dbf57fe3b96ae8cbb91cc6a8aa
HarshitaBrandshow <harshita.trainee@brandshow.in>
2025-07-22 16:27:08 +0530

    remove comment

--- src/utills/ImageUpload.js
+++ src/utills/ImageUpload.js
@@ -36,11 +36,11 @@ export async function uploadImages(files, folderName = "") {
     return data.data.fileUrls.map((url) => {
       try {
         const parsedUrl = new URL(url);
-        return parsedUrl.pathname; // gives "/folder/123.png"
+        return parsedUrl.pathname;
       } catch {
         // fallback if not a full URL
         const parts = url.split("/");
-        return "/" + parts.slice(-2).join("/"); // fallback to "/folder/filename"
+        return "/" + parts.slice(-2).join("/"); 
       }
     });
   } catch (error) {

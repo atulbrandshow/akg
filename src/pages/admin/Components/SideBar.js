6fd80eb1a3ff6688b5d0288617d381f815d971c9
atul-xt <atul.xtt@gmail.com>
2025-07-28 11:16:18 +0530

    fixed

--- src/pages/admin/Components/SideBar.js
+++ src/pages/admin/Components/SideBar.js
@@ -283,7 +283,7 @@ export default function SideBar({ onLogout }) {
 
               {/* Collapsible Section Items */}
               <div
-                className={`overflow-hidden transition-all duration-300 ${expandedNavSections[sectionIndex] ? "max-h-96 opacity-100" : "max-h-0 opacity-0"
+                className={`overflow-y-auto scrollbar-hidden transition-all duration-300 ${expandedNavSections[sectionIndex] ? "max-h-96 opacity-100" : "max-h-0 opacity-0"
                   }`}
               >
                 <ul className="space-y-2 mb-4">

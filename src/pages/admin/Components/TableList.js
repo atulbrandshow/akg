da954abb27a3e3f3a7768cd984762580049bc5e0
atul-xt <atul.xtt@gmail.com>
2025-07-31 11:49:50 +0530

    empty push

--- src/pages/admin/Components/TableList.js
+++ src/pages/admin/Components/TableList.js
@@ -201,7 +201,7 @@ const TableList = ({ type, title, subTitle }) => {
                             </div>
                             <input
                                 type="text"
-                                placeholder={`Search ${type?.toLowerCase()}...`}
+                                placeholder={`Search ${type?.toLowerCase()}.....`}
                                 value={searchTerm}
                                 onChange={(e) => setSearchTerm(e.target.value)}
                                 className="block w-full pl-10 pr-3 py-2 border font-novaReg border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all duration-200"

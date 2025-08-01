cba4a40f6305ed68aad02a422f258cf6d3e066da
atul-xt <atul.xtt@gmail.com>
2025-08-01 15:48:15 +0530

    fixed

--- src/pages/[[...slug]].js
+++ src/pages/[[...slug]].js
@@ -94,7 +94,7 @@ export async function getServerSideProps(context) {
 
     // If it's home page, change path to /home
     if (path === "/") {
-        path = "/home";
+        path = "/";
     }
 
     try {

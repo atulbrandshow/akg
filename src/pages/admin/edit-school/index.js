909f5bbad972dc6ef2b66f462dde02dcd8608a24
yatishprajapatbrandshow <yatish.prajapat@brandshow.in>
2025-08-02 11:10:59 +0530

    New push

---
+++ src/pages/admin/edit-school/index.js
@@ -0,0 +1,18 @@
+import { Suspense } from "react";
+import SideBar from "../Components/SideBar";
+import EditDynamicPages from "../Components/EditDynamicPages";
+
+export default function Home() {
+  return (
+    <>
+      <div className="flex bg-gray-100">
+        <SideBar />
+        <div className="pt-10 px-10 overflow-x-auto w-full h-screen">
+          <Suspense fallback={<div>Loading...</div>}>
+            <EditDynamicPages type="School" componentType="school-details" />
+          </Suspense>
+        </div>
+      </div>
+    </>
+  );
+}

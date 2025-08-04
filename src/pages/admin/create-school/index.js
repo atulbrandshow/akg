ec6131d58c099d5547a9d0632e7995523ee3ba56
atul-xt <atul.xtt@gmail.com>
2025-08-04 11:50:39 +0530

    fixed

--- src/pages/admin/create-school/index.js
+++ src/pages/admin/create-school/index.js
@@ -1,4 +1,3 @@
-import CreateDynamicPages from "../Components/CreateDynamicPages";
 import CreateSchoolPages from "../Components/CreateSchoolPages";
 import SideBar from "../Components/SideBar";
 
@@ -8,7 +7,7 @@ export default function Home() {
       <div className="flex bg-gray-100">
         <SideBar />
         <div className="pt-10 overflow-x-auto w-full h-screen px-10">
-          <CreateSchoolPages type="School" componentType="school-details" parentId={753006} />
+          <CreateSchoolPages type="School" componentType="school-details" parentId={0} />
         </div>
       </div>
     </>

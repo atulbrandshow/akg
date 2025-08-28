864a67b03176d4b577af76e19cc2b922e9fd36da
atul-xt <atul.xtt@gmail.com>
2025-08-28 12:31:59 +0530

    fixed

--- src/Components/SchoolHeader.js
+++ src/Components/SchoolHeader.js
@@ -20,18 +20,19 @@ const SchoolHeader = ({ heading, desc, gradientColors, banner, data }) => {
     const gradientStyle = {
         background: `linear-gradient(to right, ${gradientColors.join(', ')})`,
     };
+    
     return (
-        <section className={`relative ${banner} bg-cover max-lg:bg-right bg-top h-[70vh] lg:h-[85vh] w-full text-white`}>
-            <div className='absolute inset-0 max-lg:w-full opacity-75' style={gradientStyle}></div>
-            <div className='max-w-[1400px] mx-auto relative z-10 flex items-end justify-center h-full'>
-                <div className='grid grid-cols-2 gap-10 pb-10 max-lg:grid-cols-1'>
-                    <div className='pb-10 px-5 max-lg:pb-0'>
-                        <h2 className='text-4xl max-lg:text-3xl max-md:text-2xl  font-novaReg leading-10'>{heading}</h2>
-                        <p className='font-novaReg mt-4 leading-5'>{desc}</p>
-                        <ul className='grid grid-cols-2 gap-5 mt-16'>
+        <section className={`relative ${banner} bg-cover bg-center h-[70vh] lg:h-[85vh] w-full text-white`}>
+            <div className='absolute inset-0 opacity-75' style={gradientStyle}></div>
+            <div className='max-w-[1440px] mx-auto relative z-10 h-full flex items-end justify-start'>
+                <div className='grid grid-cols-1 lg:grid-cols-2 gap-10 px-5 lg:px-10'>
+                    <div className='flex flex-col justify-center h-full pb-10 md:pb-20'>
+                        <h2 className='text-2xl lg:text-3xl xl:text-4xl font-novaReg leading-tight lg:leading-snug'>{heading}</h2>
+                        <p className='font-novaReg mt-4 lg:text-lg leading-snug max-w-2xl'>{desc}</p>
+                        <ul className='grid grid-cols-1 sm:grid-cols-2 gap-3 md:gap-5 mt-10 lg:mt-16'>
                             {items.map((item, index) => {
                                 return (
-                                    <li key={index} className='flex items-center gap-2'>
+                                    <li key={index} className='flex items-center gap-3'>
                                         {item.icon}
                                         <p className='font-novaReg'>{item.title}</p>
                                     </li>
